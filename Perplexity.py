1a3cdc7945f3783e4d90db2c841f0ad4623a01f3
Shane <shane.morgan@itechpros.com.au>
2023-08-15 14:03:04 +1000

    Update Perplexity.py
    Fixed websocket reconnection logic.

--- Perplexity.py
+++ Perplexity.py
@@ -2,14 +2,12 @@ from time import sleep
 from uuid import uuid4
 from requests import Session
 from threading import Thread
-from time import sleep, time
+from time import time, sleep
 from json import loads, dumps
 from random import getrandbits
 from websocket import WebSocketApp
 import websocket
 import ssl
-
-#from Answer import Answer
 
 class Perplexity:
     """A class to interact with the Perplexity website.
@@ -33,15 +31,31 @@ class Perplexity:
         self.auth_session()
         self.query_str = ""
         self.answer = ""
+        self.testcloseconnection = 1
         
         #Available Models
         # llama-2-7b-chat
         # llama-2-13b-chat
         # llama-2-70b-chat
-        self.model = "llama-2-70b-chat"
+        self.model = "llama-2-13b-chat"
 
         sleep(1)
+        
+    def reconnect(self):
+        print("Reconnecting websocket...")
 
+        # Reinitialize the WebSocket
+        self.session: Session = self.init_session()
+        self.searching = False
+        self.t: str = self.get_t()
+        self.sid: str = self.get_sid()
+        self.frontend_uuid = str(uuid4())
+        self.frontend_session_id = str(uuid4())
+
+        assert self.ask_anonymous_user(), "Failed to ask anonymous user"
+        self.ws: WebSocketApp = self.init_websocket()
+        
+        
     def init_session(self) -> Session:
         session: Session = Session()
 
@@ -110,8 +124,9 @@ class Perplexity:
             elif message == "3probe":
                 self.ws.send("5")
             elif message == "6":
-                if self.ws_message != "":
-                    self.ws.send(self.ws_message)
+                if self.ws:
+                    if self.ws_message != "":
+                        self.ws.send(self.ws_message)
 
             if (self.searching) and message.startswith(f"42[\"{self.model}_query_progress"):
                 # Check if the string contains '"status":"completed"' and '"final":true'
@@ -128,12 +143,12 @@ class Perplexity:
     def on_close(self, ws, close_status_code, close_msg):
         print("Websocket connection closed.", close_status_code, close_msg)
         self.ws_connected = False
-        while self.answer is None:
-            self.ws: WebSocketApp = self.init_websocket()
-            self.answer = self.search(self.query_str)
+        self.reconnect()  # Call reconnect method
+
 
     def on_error(self, ws, error):
         print(f"Websocket error: {error}")
+        self.reconnect()  # Call reconnect method
     
     def init_websocket(self) -> websocket.WebSocketApp:       
         headers = {
@@ -145,7 +160,7 @@ class Perplexity:
             "Cookie": self.get_cookies_str()
         }
 
-        ws = WebSocketApp(
+        self.ws = WebSocketApp(
             url=f"wss://labs-api.perplexity.ai/socket.io/?EIO=4&transport=websocket&sid={self.sid}",
             header=headers,
             on_open=self.on_open,
@@ -153,55 +168,47 @@ class Perplexity:
             on_error=self.on_error,
             on_close=self.on_close,
         )
-        ws_thread = Thread(target=ws.run_forever, kwargs={'sslopt': {"cert_reqs": ssl.CERT_NONE}})
+        ws_thread = Thread(target=self.ws.run_forever, kwargs={'sslopt': {"cert_reqs": ssl.CERT_NONE}})
         ws_thread.daemon = True
         ws_thread.start()
-        return ws
+        return self.ws
 
     def auth_session(self) -> None:
         self.session.get(url="https://www.perplexity.ai/api/auth/session")
 
-    def search(self, query: str, retry_count=0):
-        if retry_count > 3:  # Maximum of 3 retries
-            self.searching = False  # Reset the searching flag
-            return 'Failed to get response after 3 retries.'
-
+    def search(self, query: str):
         formatted_query = query.replace('\n', '\\n').replace('\t', '\\t')
         self.query_str = formatted_query
 
         # If already searching, return an error, reset the searching flag, and retry
         if self.searching:
+            print("Already searching...")
             self.searching = False
-            return self.search(query, retry_count + 1)
+            return self.search(query)
 
         self.searching = True
         self.n += 1
         self.ws_message: str = f'42["perplexity_playground",{{"model":"{self.model}","messages":[{{"role":"user","content":"{formatted_query}","priority":0}}]}}]'
-        start_time = time()
-        timeout = 40  # Timeout in seconds
-
+            
         # Waiting for connection to open
         while not self.ws.sock or not self.ws.sock.connected:
             print("Waiting for connection to open...")
-            if time() - start_time > timeout:
-                self.ws: WebSocketApp = self.init_websocket()
-                return self.search(query, retry_count + 1)
             sleep(1)
 
         self.ws.send(self.ws_message)
 
         # Waiting for search to complete
-        start_time = time()
         while self.searching:
             #print("Searching...")
-            if time() - start_time > timeout:
-                return self.search(query, retry_count + 1)
             sleep(1)
 
         # Process the response
         if self.answer != "":
             formatted_output = self.answer.replace('\\n', '\n').replace('\\t', '\t')
+            self.testcloseconnection += 1
             return formatted_output
         else:
             self.searching = False  # Reset the searching flag before retrying
-            return self.search(query, retry_count + 1)  # Recursive call if there is an error
+            return self.search(query)  # Recursive call if there is an error
+
+        
